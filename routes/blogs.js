1446832e48be6b7e4b10b69da5b0630a440c2d22
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-05-17 18:57:46 -0500

    update submit

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,22 +1,51 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var blogs = require("../public/javascripts/sampleBlogs")
-const blogPosts = blogs.blogPosts
+var blogs = require("../public/javascripts/sampleBlogs");
+const blogPosts = blogs.blogPosts;
 
 /* GET Blogs listing. */
-router.get('/', function(req, res, next) {
-  res.render('blogs', {title: 'Blogs'});
+router.get("/", function (req, res, next) {
+  res.render("blogs", { title: "Blogs" });
 });
 
-router.get('/all', function(req, res, next) {
-  res.json(blogPosts);
+router.get("/all", function (req, res, next) {
+  let sort = req.query.sort;
+  res.json(sortBlogs(sort));
 });
 
-router.get('/:blogId', (req, res) => {
+router.get("/get-by-id/:blogId", (req, res) => {
   console.log(req.params);
-  let id = req.params.id;
-  res.json(`${id}`);
-})
+  const blogId = req.params.blogId;
+  //JSON: Javascript Object Notation
+  res.json(findBlogId(blogId));
+});
 
 module.exports = router;
+
+/* HELPER FUNCTIONS */
+
+// Find Blog Id
+let findBlogId = (id) => {
+  for (let i = 0; i < blogPosts.length; i++) {
+    let blog = blogPosts[i];
+    if (blog.id === id) {
+      return blog;
+    }
+  }
+};
+
+// Sort Blog Array into ascending or descending order
+let sortBlogs = (order) => {
+  if (order === "asc") {
+    return blogPosts.sort(function (a, b) {
+      return new Date(a.createdAt) - new Date(b.createdAt);
+    });
+  } else if (order === "desc") {
+    return blogPosts.sort(function (a, b) {
+      return new Date(b.createdAt) - new Date(a.createdAt);
+    });
+  } else {
+    return blogPosts;
+  }
+};
